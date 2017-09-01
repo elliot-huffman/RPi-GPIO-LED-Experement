702011b7174adbbc237fa13d0ba1738450a295e1
Elliot Huffman <ehuffman@elliot-labs.com>
2017-09-01 16:17:32 -0400

    Blink LED
    Blinks an LED every second

--- LED_Blink.py
+++ LED_Blink.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+import time
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(2, GPIO.OUT)
+
+while True:
+    GPIO.output(2, 0)
+    time.sleep(1)
+    GPIO.output(2, 1)
+    time.sleep(1)
+exit()
